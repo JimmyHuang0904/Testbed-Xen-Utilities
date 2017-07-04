24621b25426e1d4368e4778afe1c008bd6ae1105
jimmyhuang0904 <jimmyhuang0904@hotmail.com>
2017-07-04 16:31:24 -0700

    USB attach on SSH connection fixed

--- USB_Attach_VM2.sh
+++ USB_Attach_VM2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-USER=jenkins
-
 Host_IP="$(ifconfig | grep -A 1 'xenbr0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 echo "$(date)" >> /tmp/USB_Auto_Attach_Logs
 echo "USB_Attach_VM2" >> /tmp/USB_Auto_Attach_Logs
@@ -13,24 +11,10 @@ IPToSet=($(ip neighbor | grep "$MacAddress" | cut -d" " -f1)) #this doesnt work 
 #Number of Bus ID for $IPToSet
 NumIDs=$(awk '{print NF}' ~/Documents/Mapping | sort -nu | tail -n 1)
 
-echo "NumIDs + 1 (Including hostname)  = $NumIDs"
-
-for (( i=2; i <= NumIDs; i++))
+for (( i=2; i <= $NumIDs; i++))
 do
-    sudo usbip bind -b $(awk -v i=$i '{print $i }' ~/Documents/Mapping | head -n 1)
+    BusID=$(awk -v i=$i '{print $i}' ~/Documents/Mapping | head -n 1)
+    echo "Binding $BusID to VM $VMToSet@$IPToSet"
+    sudo usbip bind -b $BusID    
+    ssh -o StrictHostKeyChecking=no -t jenkins@$IPToSet "sudo usbip attach --remote=$Host_IP --busid=$BusID" && echo "Attached the BusID $BusID to the VM $VMToSet@$IPToSet" >> /tmp/USB_Auto_Attach_Logs
 done
-
-echo "Host_IP = $Host_IP"
-echo "VMToSet = $VMToSet"
-echo "MacAddress = $MacAddress"
-echo "IPToSet = $IPToSet"
-
-ssh -o StrictHostKeyChecking=no -l $USER@$IPToSet
-
-#for (( i=2; i <= NumIDs; i++))
-#do
-    
-#done
-#ssh -t jenkins@$IPToSet "sudo usbip --port | grep $BusID" && { echo "ERROR: The BusID $BusID has already been attached to the VM $VMToSet@$IPToSet" >> /tmp/USB_Auto_Attach_Logs; exit 1; }
-#usbip bind -b "$BusID" || { echo "Error binding the BusID $BusID" >> /tmp/USB_Auto_Attach_Logs; exit 1; }
-#ssh -t jenkins@$IPToSet "sudo usbip --attach  $Host_IP -b $BusID" && echo "Attached the BusID $BusID to the VM $VMToSet@$IPToSet" >> /tmp/USB_Auto_Attach_Logs
