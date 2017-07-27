6d6b766549e147d21cac61e77a76642ca3370f4b
jimmyhuang0904 <jimmyhuang0904@hotmail.com>
2017-07-27 14:51:22 -0700

    start up env for VM1 to VM4

--- Startupenv.sh
+++ Startupenv.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-sudo losetup /dev/loop1 /mnt/vmdisk/ubuntu16_04_1 
-sudo losetup /dev/loop2 /mnt/vmdisk/ubuntu16_04_2
+sudo losetup /dev/loop1 /mnt/vmdisk/ubuntu160401 
+sudo losetup /dev/loop2 /mnt/vmdisk/ubuntu160402
 sudo losetup /dev/loop3 /mnt/vmdisk/ubuntu160403
-sudo losetup /dev/loop4 /mnt/vmdisk/ubuntu
+sudo losetup /dev/loop4 /mnt/vmdisk/ubuntu160404
+sudo losetup /dev/loop5 /mnt/vmdisk/ubuntu
+
+sudo xl create /etc/xen/ubuntu160401.cfg
+sudo xl create /etc/xen/ubuntu160402.cfg
+sudo xl create /etc/xen/ubuntu160403.cfg
+sudo xl create /etc/xen/ubuntu160404.cfg
 
 sudo modprobe usbip-core
 sudo modprobe usbip-host
