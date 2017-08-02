db2e292b713eac2b1aed3196faa174365252af10
Jimmy Huang <jihuang@sierrawireless.com>
2017-08-02 11:53:04 -0700

    Add boot functionality

--- Arduino/powerswitch.py
+++ Arduino/powerswitch.py
@@ -13,45 +13,17 @@ serialref = serial.Serial(
     timeout=5
 )
 
+def bootswitch():
+    serialref.write('x03\n')
+    time.sleep(1)
+    serialref.write('x02\n')
+    return
+
 if __name__ == "__main__":
-    hexwrite = raw_input('Enter hex value (eg. x0E) ')
-    serialref.write(hexwrite+'\n')
+    hexwrite = raw_input('Enter hex value (eg. x0E) or boot:  ')
+    if hexwrite == 'boot':
+        bootswitch()
+    else:
+        serialref.write(hexwrite+'\n')
     serialref.close()
-
-#serialref.write(bytes(b'asdf'))
-#out = ''
-# let's wait one second before reading output (let's give device time to answer)
-#time.sleep(1)
-#while serialref.inWaiting() > 0:
-#    out += ser.read(40)
-
-#if out != '':
-#    print(">>" + out)
-
-
-#serialref.close()
-
-# print z1serial  # debug serial.
-
-#def relay_1():
-#    serialref.write("00".encode)
-#    time.sleep(1)
-#    serialref.write("01".encode)
-
-#print serialref.is_open  # True for opened
-#if serialref.is_open:
-#    serialref.write('0f')
-#    while True:
-#        size = z1serial.inWaiting()
-#        if size:
-#            data = z1serial.read(size)
-#            print data
-#        else:
-#            print 'no data'
-#        time.sleep(1)
-#else:
-#    print 'z1serial not open'
-
-#if __name__ == "__main__":
-#    relay_1()
 
