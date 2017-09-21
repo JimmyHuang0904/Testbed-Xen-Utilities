1fd98c495353158b985f14df74c444df55d87d07
Jimmy Huang <jihuang@sierrawireless.com>
2017-09-20 18:00:53 -0700

    Redesign into higher level of modularity

--- script/power_switch.py
+++ script/power_switch.py
@@ -1,177 +1,214 @@
-import serial
-import time
-import string
+#!/usr/bin/env python2
+
 import sys
+import time
+import socket
 import os
-import json
 
-pathToJson = "/home/jimmy/pythonWorkspace/power_switch/power_switch.json"
-baudrate = 9600
+def write_to_json(data, path):
+  import json
+  from pprint import pprint
+  with open(path, "w") as data_file:
+    json.dump(data, data_file, sort_keys=True, indent=4, separators=(',', ': '))
 
-class RelayControl:
+def read_json(path):
+  import json
+  with open(path) as data_file:
+    return json.load(data_file)
 
-    power_bit  = 0x01
-    reboot_bit = 0x02
-    sim_bit    = 0x04
+class Target:
+  def set_boot_inverted(self, state):
+    print "Target#%d: Gpio Boot %s" % (self.id, "Inverted" if state else "Normal")
+    self.boot_inverted = state
 
-    def initialize_relay(self):
-        self.serialref.write('\x50')
-        time.sleep(0.5)
-        self.serialref.write('\x51')
-        time.sleep(0.5)
+  def is_boot_inverted(self):
+    return self.boot_inverted
 
-    def reboot(self):
-        time.sleep(0.5)
-        self.next_state = chr(self.power_bit | self.reboot_bit)
-        self.serialref.write(self.next_state)
+  def output(self, gpio, state):
+    raise Exception("Not implemented")
 
-        time.sleep(0.5)
-        self.next_state = chr(self.reboot_bit)
-        self.serialref.write(self.next_state)
+  def input(self, gpio):
+    raise Exception("Not implemented")
 
-    def power_on(self):
-        time.sleep(0.5)
-        self.next_state = chr(not self.power_bit)
-        self.serialref.write(self.next_state)
+  def switch_power(self, state):
+    print "Target#%d: Power %s (is %s)" \
+      % (self.id, "On" if state else "Off", "Off" if self.input(self.power) else "On")
+    self.output(self.power, state)
 
-    def power_off(self):
-        time.sleep(0.5)
-        if not self.current_state:
-            self.next_state = chr(self.power_bit)
-        else:
-            self.next_state = chr(self.power_bit)
+  def switch_boot(self, state):
+    actual_state = not state
+    if self.is_boot_inverted():
+      actual_state = state
 
-        self.serialref.write(self.next_state)
+    print "Target#%d: Boot %s (is %s)" \
+      % (self.id, "On" if actual_state else "Off", "Off" if self.input(self.boot) else "On")
+    self.output(self.boot, actual_state)
 
-    def switch_power(self):
-        time.sleep(0.5)
-        if not self.current_state:
-            self.next_state = chr(self.power_bit)
-        else:
-            if int(self.current_state, 16) & self.power_bit:
-                self.next_state = chr(~self.power_bit & int(self.current_state, 16))
-            else:
-                self.next_state = chr( self.power_bit | int(self.current_state, 16))
+  def switch_sim(self, state):
+    if self.sim == -1:
+      print "Target#%d: SIM switch is NOT SUPPORTED" % (self.id)
+    else:
+      self.__check_gpio(self.sim)
+      print "Target#%d: SIM %s (is %s)" \
+        % (self.id, "On" if state else "Off", "Off" if self.input(self.sim) else "On")
+      self.output(self.sim, state)
 
-        self.serialref.write(self.next_state)
+class UsbRelayTarget(Target):
 
-    def switch_sim(self):
-        time.sleep(0.5)
-        if not self.current_state:
-            print("no state")
-            self.next_state = chr(self.sim_bit)
-        else:
-            if int(self.current_state, 16) & self.sim_bit:
-                self.next_state = chr(~self.sim_bit & int(self.current_state, 16))
-            else:
-                self.next_state = chr( self.sim_bit | int(self.current_state, 16))
+  path_to_json =  os.getenv('POWER_SWITCH_JSON','/tmp/power_switch.json')
+  baudrate = 9600
 
-        self.serialref.write(self.next_state)
+  def __init__(self, id, power, boot, sim=-1):
 
+    import json
+    import serial
 
-class Target(RelayControl):
+    self.id = id
+    self.power = power
+    self.boot = boot
+    self.sim = sim
 
-    def __init__(self, path):
-        ''' Read JSON file configurations '''
-        with open(path) as data_file:    
-            self.json_data = json.load(data_file)
+    self.json_data = read_json(self.path_to_json)
 
-    def set_target_id(self, target_id):
-        self.target_id = target_id
+    self.device = self.json_data["target"][self.id - 1]
 
-    def write_to_json(self, data):
-        with open(pathToJson, "w") as data_file:
-            json.dump(data, data_file)
+    self.open_port()
 
-    def set_polarity_inverted(self, state):
-        self.polarity_inverted = state
+    self.current_state = self.device["overall_state"]
 
-    # def is_polarity_inverted(self, polarity_bit):
-        # return self.polarty_inverted
+    self.get_position()
 
-    def open_port(self):
-        for file in os.listdir("/sys/bus/usb/devices" + self.json_data["bus"][self.target_id - 1]["num"]):
-            if file.startswith("ttyUSB"):
-                port = "/dev/" + file
-                self.serialref = serial.Serial(port, baudrate)
+    os.chdir("/sys/bus/usb/devices" + self.device["busnum"])
+    os.chdir("..")
+    with open('devnum', 'r') as devnumfile:
+      devnum = int( devnumfile.read() )
 
-    def dev_num_check(self):
-        ''' Change directory to where devnum textfile is and save it to self.devnum'''
-        os.chdir("/sys/bus/usb/devices" + self.json_data["bus"][self.target_id - 1]["num"])
-        os.chdir("..")
-        devnumfile = open('devnum', 'r')
-        devnum = int( devnumfile.read() )
+      if devnum != self.device["devnum"]:
+        self.initialize_relay()
 
-        if devnum != self.json_data["bus"][self.target_id - 1]["devnum"]:
-            # Running initializing sequence
-            print("Initializing device")
-            self.initialize_relay()
+        # Overwriting new devnum to JSON file
+        self.device["devnum"] = devnum
+        write_to_json(self.json_data, self.path_to_json)
+      devnumfile.close()
 
-            # Overwriting new devnum to JSON file
-            self.json_data["bus"][self.target_id - 1]["devnum"] = devnum
-            self.write_to_json(self.json_data)
-        devnumfile.close()
+  def get_position(self):
 
-    def get_current_state(self):
-        self.current_state = self.json_data["state"]
+    for functions in self.device["gpio"]:
+      if functions["name"] == 'power':
+        self.power = functions["position"]
+      elif functions["name"] == 'boot':
+        self.boot = functions["position"]
+      elif functions["name"] == 'sim':
+        self.sim = functions["position"]
+      else:
+        raise Exception("GPIO missing keyword \"name\" in JSON file")
 
-    def set_state(self):
-        self.json_data["state"] = hex(ord(self.next_state))
-        self.write_to_json(self.json_data)
+  def open_port(self):
+    import serial
 
-# main
+    for file in os.listdir("/sys/bus/usb/devices" + self.device["busnum"]):
+      if file.startswith("ttyUSB"):
+        port = "/dev/" + file
+        print(port)
+
+        self.serial_ref = serial.Serial(port, self.baudrate)
+
+  def initialize_relay(self):
+    print "Initializing Usb Relay Device"
+    self.serial_ref.write('\x50')
+    time.sleep(1)
+    self.serial_ref.write('\x51')
+    time.sleep(1)
+
+    self.next_state = chr(0x00)
+    self.set_state()
+
+  def input(self, gpio):
+    state = self.device["gpio"][gpio]["state"]
+    return state
+
+  def output(self, gpio, state):
+    time.sleep(1)
+
+    bit = 1 << gpio
+
+    self.current_state = self.device["overall_state"]
+
+    if not self.current_state:
+      if state:
+        self.next_state = chr(bit)
+      else:
+        self.next_state = chr(0x00)
+    elif state:
+      self.next_state = chr( bit | int(self.current_state, 16))
+    else:
+      self.next_state = chr(~bit & int(self.current_state, 16))
+
+    self.serial_ref.write(self.next_state)
+    self.set_state()
+
+  def set_state(self):
+    self.device["overall_state"] = hex(ord(self.next_state))
+    write_to_json(self.json_data, self.path_to_json)
+
+targets = {}
+
+hostname = os.getenv("POWER_CONFIG", socket.gethostname())
+
+print "hostname: %s" % (hostname)
+
+if hostname == "usb_relay":
+  targets[1] = UsbRelayTarget(1, 0, 1, 2)
+else:
+  raise Exception("Hostname has no permission")
+
 if len(sys.argv) < 3:
-    sys.stderr.write('Usage: <target> <fn> <?boot_inverted 1 or 0>\n')
-    sys.exit(1)
+  sys.stderr.write('Usage: <target> <fn> <?boot_inverted 1 or 0>\n')
+  sys.exit(1)
 
 target_id = int(sys.argv[1])
 fn = sys.argv[2]
 
-polarity_inverted = True
+boot_inverted = True
 if len(sys.argv) >= 4:
-    polarity_inverted = (sys.argv[3] == "1")
+  boot_inverted = (sys.argv[3] == "1")
 
-var = Target(pathToJson)
+if not target_id in targets:
+  sys.stderr.write('Target %d unknown\n' % target_id)
+  sys.exit(1)
 
-var.set_target_id(target_id)
-
-var.open_port()
-
-var.get_current_state()
-
-print(var.current_state)
-# var.is_polarity_inverted(var.serialref)
-
-var.dev_num_check()
+target = targets[target_id]
+target.set_boot_inverted(boot_inverted)
 
 if fn == "reboot":
-    var.reboot()
+  target.switch_boot(True)
+  target.switch_power(False)
+  time.sleep(1)
+  target.switch_power(True)
 
-elif fn == "off":
-    var.power_off()
+if fn == "recovery":
+  target.switch_boot(False)
+  target.switch_power(False)
+  time.sleep(1)
+  target.switch_power(True)
 
 elif fn == "on":
-    var.power_on()
+  target.switch_boot(True)
+  target.switch_power(True)
 
-elif fn == "sim":
-    var.switch_sim()
+elif fn == "off":
+  target.switch_power(False)
 
-elif fn == "power":
-    var.switch_power()
+elif fn == "power" or fn == "sim":
+  arg_state = sys.argv[3]
+  if arg_state == "on":
+    state = True
+  elif arg_state == "off":
+    state = False
+  else:
+    raise Exception("Invalid state %s" % arg_state)
 
-
-var.set_state()
-
-
-    # # hexwrite = sys.argv[1]
-    # # print(hexwrite)
-    # # table = string.maketrans("abcdefABCDEF", "jklmnoJKLMNO")
-    # bootswitch()
-    # # hexwrite = hexwrite.translate(table)
-    # # if hexwrite == 'boot':
-    # #     bootswitch()
-    # # elif hexwrite == 'cycle':
-    # #     powercycle()
-    # # else:
-    # #     serialref.write('\\'+hexwrite)
-    # serialref.close()
+  if fn == "power":
+    target.switch_power(state)
+  elif fn == "sim":
+    target.switch_sim(state)
